4523769f979cbe473819608e17016c4039e1e364
Greg <gregorytlivingston@gmail.com>
2021-09-14 22:09:51 -0400

    greg pushing up grid board

--- src/routes/Test.tsx
+++ src/routes/Test.tsx
@@ -1,10 +1,28 @@
 import React, { useEffect, useState } from 'react'
+import MainBoard from '../components/MainBoard';
 import Project from '../model/Project';
 import { fetchProjects } from '../services/ProjectService'
 
 function Test() {
     const [ projects, setProjects ] = useState<Project[]>([]);
     useEffect(loadProjects, [])
+
+    const [fakeData, setFakeData] = useState([
+        {   name: "Mow the Lawn",
+            description: "Lots of Grass",
+            outdoor: true,
+            priority: 1
+        },
+        {
+            name: "Paint the Master",
+            description: "Lots of Paint",
+            outdoor: false,
+            priority: 1
+        }
+     
+    ])
+
+
   
     function loadProjects() {
       fetchProjects().then(setProjects);
@@ -13,10 +31,7 @@ function Test() {
 
     return (
         <div className="TestRoute">
-            <p>Find Your Projects Here</p>
-            {projects.map((project, index) =>
-            <p key={`${index}-${project.name}`}>{project.name}</p>    
-            )}
+            <MainBoard data={fakeData}/>
         </div>
     )
 }
